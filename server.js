efec555f1881020e180b816867d37b10085be991
Temmycoder <oladutemutemiloluwa@icloud.com>
2026-01-17 17:58:18 +0000

    the project works

--- server.js
+++ server.js
@@ -5,18 +5,31 @@ const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
+let sender = null;
+let receiver = null;
+
 io.on("connection", socket => {
-  socket.on("offer", data => {
-    socket.broadcast.emit("offer", data);
+
+  socket.on("join", role => {
+    if (role === "sender") sender = socket;
+    if (role === "receiver") {
+      receiver = socket;
+      sender?.emit("receiver-ready");
+    }
   });
 
-  socket.on("answer", data => {
-    socket.broadcast.emit("answer", data);
+  socket.on("offer", offer => receiver?.emit("offer", offer));
+  socket.on("answer", answer => sender?.emit("answer", answer));
+
+  socket.on("ice", candidate => {
+    if (socket === sender) receiver?.emit("ice", candidate);
+    else sender?.emit("ice", candidate);
   });
 
-  socket.on("ice", data => {
-    socket.broadcast.emit("ice", data);
+  socket.on("disconnect", () => {
+    if (socket === sender) sender = null;
+    if (socket === receiver) receiver = null;
   });
 });
 
-http.listen(3000, () => console.log("server running on port 3000"));
+http.listen(3000, () => console.log("Server running on port 3000"));
